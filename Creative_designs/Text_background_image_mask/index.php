d05e63696b83115ae658aa8d616722c33fd00e4d
coder.roby <coder.roby@gmail.com>
2024-09-08 14:13:51 +0600

    added a design image masking in text

---
+++ Creative_designs/Text_background_image_mask/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #000;
+            margin: 0 auto;
+        }
+
+        .first_div {
+            width: 60%;
+            height: 100px;
+            margin: 0 auto;
+            align-items: center;
+            text-align: center;
+        }
+
+        .first_div h1 {
+            font-size: 10em;
+            text-transform: uppercase;
+            color: transparent;
+            font-family: fantasy;
+            letter-spacing: 10px;
+            padding: 15px;
+            font-weight: 500;
+            /* background-image: url(images/sea.jpg); */
+            background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ718nztPNJfCbDJjZG8fOkejBnBAeQw5eAUA&s);
+            background-image: url(https://img.freepik.com/premium-photo/lepard-is-sitting-branch_811396-4434.jpg);
+            background-size: contain;
+            /* background-size: inherit; */
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            background-position-y: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="first_div">
+        <H1 class="h1_css">
+            TIGER
+        </H1>
+        </div->
+</body>
+
+</html>
